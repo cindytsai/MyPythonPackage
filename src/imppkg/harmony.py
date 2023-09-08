e581186986bfa1e4bf9914adf1bb2e9f3824cf9e
cindytsai <turquoisea.tsai@gmail.com>
2023-09-07 20:45:43 -0500

    Part 2 - Section 4.4: Adding dependencies.

--- src/imppkg/harmony.py
+++ src/imppkg/harmony.py
@@ -1,6 +1,6 @@
 import sys
 import imppkg.harmonic_mean
-
+import termcolor
 
 def main():
     num_list = []
@@ -8,10 +8,11 @@ def main():
         try:
             num_list.append(float(sys.argv[i]))
         except ValueError:
-            print("Cannot convert {} to float ... ignoring it".format(sys.argv[i]))
+            termcolor.cprint("Cannot convert {} to float ... ignoring it".format(sys.argv[i]),
+                             "red", "on_blue", attrs=["underline"])
 
     try:
         result = imppkg.harmonic_mean.harmonic_mean(num_list)
-        print(result)
+        termcolor.cprint(result, "green", "on_cyan", attrs=["bold"])
     except ZeroDivisionError:
-        print("ZeroDivisionError")
+        termcolor.cprint("ZeroDivisionError", "red", "on_blue", attrs=["blink"])
