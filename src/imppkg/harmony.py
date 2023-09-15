987e6a4d4dc5c9f89efad96af8dd026906177275
cindytsai <turquoisea.tsai@gmail.com>
2023-09-14 21:45:20 -0500

    Part 2 - Section 5.1.3: Increasing test coverage

--- src/imppkg/harmony.py
+++ src/imppkg/harmony.py
@@ -4,16 +4,15 @@ import termcolor
 
 
 def main():
-    num_list = []
-    for i in range(1, len(sys.argv)):
-        try:
-            num_list.append(float(sys.argv[i]))
-        except ValueError:
-            termcolor.cprint("Cannot convert {} to float ... ignoring it".format(sys.argv[i]),
-                             "red", "on_blue", attrs=["underline"])
+    result = 0.0
 
     try:
-        result = imppkg.harmonic_mean.harmonic_mean(num_list)
-        termcolor.cprint(result, "green", "on_cyan", attrs=["bold"])
-    except ZeroDivisionError:
-        termcolor.cprint("ZeroDivisionError", "red", "on_blue", attrs=["blink"])
+        nums = [float(num) for num in sys.argv[1:]]
+        try:
+            result = imppkg.harmonic_mean.harmonic_mean(nums)
+            termcolor.cprint(result, "yellow", "on_cyan", attrs=["bold"])
+        except ZeroDivisionError:
+            termcolor.cprint("ZeroDivisionError", "red", "on_blue", attrs=["blink"])
+    except ValueError:
+        nums = []
+        termcolor.cprint("ValueError", "red", "on_blue", attrs=["underline"])
--- test/test_harmonic_mean.py
+++ test/test_harmonic_mean.py
@@ -1,4 +1,43 @@
+import sys
+from termcolor import colored
 from imppkg.harmony import main
+
+
+def test_harmony_happy_path(monkeypatch, capsys):
+    # patching fake inputs
+    inputs = ["1", "4", "4"]
+    monkeypatch.setattr(sys, "argv", ["harmony"] + inputs)
+
+    # get inputs from sys.argv and test it
+    main()
+
+    # check the value
+    expected_value = 2.0
+    assert float(capsys.readouterr().out.strip()) == colored(expected_value, "yellow", "on_cyan", attrs=["bold"])
+
+
+def test_harmony_unhappy_path_value_error(monkeypatch, capsys):
+    # patching fake inputs
+    inputs = ["1", "a"]
+    monkeypatch.setattr(sys, "argv", ["harmony"] + inputs)
+
+    # get inputs from sys.argv and test it
+    main()
+
+    # check the result
+    assert capsys.readouterr().out.strip() == "ValueError"
+
+
+def test_harmony_unhappy_path_zero_division_error(monkeypatch, capsys):
+    # patching fake inputs
+    inputs = ["0", "0"]
+    monkeypatch.setattr(sys, "argv", ["harmony"] + inputs)
+
+    # get inputs from sys.argv and test it
+    main()
+
+    # check the result
+    assert capsys.readouterr().out.strip() == "ZeroDivisionError"
 
 def test_always_pass():
     assert True
