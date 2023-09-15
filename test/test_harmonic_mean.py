ee669c32edbc9a114cbc5ad0a989ebf518675109
cindytsai <turquoisea.tsai@gmail.com>
2023-09-14 21:55:02 -0500

    Part 2 - Section 5.2.1: Parametrize tests.

--- test/test_harmonic_mean.py
+++ test/test_harmonic_mean.py
@@ -1,43 +1,22 @@
 import sys
-from termcolor import colored
+import pytest
 from imppkg.harmony import main
 
-
-def test_harmony_happy_path(monkeypatch, capsys):
+@pytest.mark.parametrize(
+    "input_args, expected",
+    [
+        (["1", "4", "4"], 2.0),
+        (["1", "a"], "ValueError"),
+        (["0"], "ZeroDivisionError")
+    ]
+)
+def test_harmony(input_args, monkeypatch, capsys, expected):
     # patching fake inputs
-    inputs = ["1", "4", "4"]
-    monkeypatch.setattr(sys, "argv", ["harmony"] + inputs)
+    monkeypatch.setattr(sys, "argv", ["harmony"] + input_args)
 
     # get inputs from sys.argv and test it
     main()
 
     # check the value
-    expected_value = 2.0
-    assert float(capsys.readouterr().out.strip()) == colored(expected_value, "yellow", "on_cyan", attrs=["bold"])
+    assert capsys.readouterr().out.strip() == str(expected)
 
-
-def test_harmony_unhappy_path_value_error(monkeypatch, capsys):
-    # patching fake inputs
-    inputs = ["1", "a"]
-    monkeypatch.setattr(sys, "argv", ["harmony"] + inputs)
-
-    # get inputs from sys.argv and test it
-    main()
-
-    # check the result
-    assert capsys.readouterr().out.strip() == "ValueError"
-
-
-def test_harmony_unhappy_path_zero_division_error(monkeypatch, capsys):
-    # patching fake inputs
-    inputs = ["0", "0"]
-    monkeypatch.setattr(sys, "argv", ["harmony"] + inputs)
-
-    # get inputs from sys.argv and test it
-    main()
-
-    # check the result
-    assert capsys.readouterr().out.strip() == "ZeroDivisionError"
-
-def test_always_pass():
-    assert True
