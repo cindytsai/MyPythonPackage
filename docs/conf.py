618651ef22dcb87762f84af350bafebad07fc4ae
cindytsai <turquoisea.tsai@gmail.com>
2023-10-03 22:57:32 -0500

    Part 3 - Section 8.2: Add docstring, and get package version number from metadata.

--- docs/conf.py
+++ docs/conf.py
@@ -3,12 +3,19 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+# -- Path setup --------------------------------------------------------------
+import os
+from importlib import metadata
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'pubpypack-harmony-cindytsai'
 copyright = '2023, Shin-Rong Tsai'
 author = 'Shin-Rong Tsai'
+PACKAGE_VERSION = metadata.version("pubpypack-harmony-cindytsai")
+version = PACKAGE_VERSION
+release = PACKAGE_VERSION
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -26,5 +33,5 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'press'
 html_static_path = ['_static']
--- src/imppkg/harmonic_mean.py
+++ src/imppkg/harmonic_mean.py
@@ -1,4 +1,11 @@
 def harmonic_mean(num_list):
+    """Return harmonic mean of a list of floats.
+
+    :param num_list: A list of floats containing values to calculate
+    :type num_list: list
+    :return: harmonic mean
+    :rtype: float
+    """
     value = 0.0
     for m in num_list:
         value += 1 / float(m)
