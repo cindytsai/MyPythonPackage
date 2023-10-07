7a4f39b39a99248317f04591c0b00cc6fba58562
cindytsai <turquoisea.tsai@gmail.com>
2023-10-07 13:22:53 -0500

    Part 3 - Section 8.3.1: Hook up builder-inited (Sphinx event)

--- docs/conf.py
+++ docs/conf.py
@@ -33,5 +33,31 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'press'
 html_static_path = ['_static']
+
+
+# -- Read the Doc Hook -------------------------------------------------------
+if os.environ.get("READTHEDOCS"):
+    from pathlib import Path
+
+    PROJECT_ROOT = Path(__file__).parent.parent
+    PACKAGE_ROOT = PROJECT_ROOT / "src" / "imppkg"
+
+    def run_apidoc(_):
+        from sphinx.ext import apidoc
+        apidoc.main([
+            "--force",
+            "--implicit-namespaces",
+            "--module-first",
+            "--separate",
+            "-o", str(PROJECT_ROOT / "docs" / "reference"),
+            str(PACKAGE_ROOT),
+            str(PACKAGE_ROOT / "*.c"),
+            str(PACKAGE_ROOT / "*.so"),
+        ])
+
+
+    def setup(app):
+        app.connect("builder-inited", run_apidoc)
+
